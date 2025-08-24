234277704e370f78b147b3e8fbbbe68fa6b6bf61
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2025-08-24 12:51:43 +0530

    Time: 76 ms (12.5%), Space: 272.6 MB (12.5%) - LeetHub

--- 3979-partition-array-into-k-distinct-groups/3979-partition-array-into-k-distinct-groups.cpp
+++ 3979-partition-array-into-k-distinct-groups/3979-partition-array-into-k-distinct-groups.cpp
@@ -9,23 +9,15 @@ public:
         int count=nums.size()/k;
 
 
-
-
         for(int i=0;i<nums.size();i++)
         {
             m[nums[i]]++;
         }
 
-        int maxi=0;
-
         for(auto i:m)
         {
-            maxi=max(maxi,i.second);
-
             if(i.second>count)return false;
         }
-
-       
 
         if(m.size()<k)return false;
 
