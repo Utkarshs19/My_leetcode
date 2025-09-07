6f98a38e4c673cbabf3dc984e2af4393f3029f03
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2025-09-07 19:07:34 +0530

    Time: 2587 ms (5.37%), Space: 29.7 MB (46.74%) - LeetHub

---
+++ 0839-short-encoding-of-words/0839-short-encoding-of-words.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minimumLengthEncoding(vector<string>& words) {
+        
+        if(words.size()==1)
+        return words[0].size()+1;
+
+        priority_queue<pair<int,string>> pq;
+
+        for(int i=0;i<words.size();i++)
+        {
+            pq.push({words[i].length(),words[i]});
+        }
+
+        map<string,int> m;
+
+        while(!pq.empty())
+        {
+            string s=pq.top().second;
+            pq.pop();
+            bool flag=false;
+            for(auto i:m)
+            {
+                string str=i.first;
+                if(str.substr(str.length()-s.length())==s)
+                {flag=true;
+                break;}
+            }
+            if(flag==false)
+            m[s]++;
+        }
+        int ans=0;
+        for(auto i:m)
+        {
+            cout<<i.first<<"  "<<i.second<<endl;
+
+            ans+=i.first.length();
+        }
+        ans+=m.size();
+
+        return ans;
+    }
+};
