b36bc85f136866245ff92f4235f6034824b4aa9e
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2025-09-11 07:14:49 +0530

    Time: 41 ms (5.33%), Space: 16.9 MB (27.48%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -2,31 +2,36 @@ class Solution {
 public:
     string sortVowels(string s) {
        set<char> st={'a','e','i','o','u','A','E','I','O','U' };
-        set<int> s1;
-        string str="";
+        map<char,int> m;
+        
         for(int i=0;i<s.length();i++)
         {
             if(st.find(s[i])!=st.end())
             {   
-                s1.insert(i);
-                str+=s[i];
+               m[s[i]]++;
             }
 
         }
-
-        sort(str.begin(),str.end());
-
-        int k=0;
 
         for(int i=0;i<s.length();i++)
         {
-            if(s1.find(i)!=s1.end())
-            {
-                s[i]=str[k];
-                k++;
+            if(st.find(s[i])!=st.end())
+            {   
+               for(auto &it:m)
+               {
+                    if(it.second>0){
+                    s[i]=it.first;
+                    it.second--;
+                    break;
+                    }
+               }
             }
+
         }
+
+
         return s;
-       
+
+
     }
 };
