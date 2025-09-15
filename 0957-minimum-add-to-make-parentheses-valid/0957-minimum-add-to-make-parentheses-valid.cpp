c5ecbfe85ae451fb9f50342db1c63f2be55478b3
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2025-09-15 19:11:33 +0530

    Time: 0 ms (100%), Space: 9.5 MB (13.04%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+
+        stack<char> st;
+
+        int cnt=0;
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]==')'&&!st.empty()&&st.top()=='(')
+            st.pop();
+            else if(st.empty()&&s[i]==')')
+            cnt++;
+            else
+            st.push('(');
+        }
+        
+
+        return st.size()+cnt;
+    }
+};
