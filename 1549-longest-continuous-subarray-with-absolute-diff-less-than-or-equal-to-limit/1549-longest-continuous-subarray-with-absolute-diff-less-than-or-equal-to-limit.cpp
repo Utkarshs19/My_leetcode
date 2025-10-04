698e5ef48f5e1930d178e5fd3c9a947fc732c4c9
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2025-10-04 10:45:53 +0530

    Time: 180 ms (13.61%), Space: 117.9 MB (6.8%) - LeetHub

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
-        // multiset to maintain the elements within the current window
-        multiset<int> m;
-        int i = 0,j=0, ans = 0;
 
-            while(j<nums.size()) {
+
+        int i=0,j=0;
+
+        int ans=INT_MIN;
+
+        multiset<int> m;
+
+        while(j<nums.size())
+        {
             m.insert(nums[j]);
 
-            // Check if the absolute difference between the maximum and minimum
-            // values in the current window exceeds the limit
-            while (*m.rbegin() - *m.begin() > limit) {
-                // Remove the element at the left pointer from the multiset
-                m.erase(m.find(nums[i]));
-                // Move the left pointer to the right to exclude the element
-                // causing the violation
-                ++i;
+            
+
+            while((*m.rbegin()-*m.begin())>limit  && i<j)
+            {
+                auto it=m.find(nums[i]);
+
+                m.erase(it);
+
+               
+
+                i++;
+
             }
 
-            // Update maxLength with the length of the current valid window
-            ans = max(ans, j - i + 1);
+            ans=max(ans,j-i+1);
             j++;
         }
 
         return ans;
+        
     }
 };
