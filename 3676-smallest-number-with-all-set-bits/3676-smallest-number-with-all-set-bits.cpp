df29681602253098e3ee59a507c4cc7ef7eb7a7e
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2025-10-29 05:35:13 +0530

    Time: 0 ms (100%), Space: 8.4 MB (30.21%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int smallestNumber(int n) {
 
-        vector<int> v;
 
         int k=0;
         int ans=0;
@@ -15,15 +14,6 @@ public:
         }
         
         
-        // for(int i=0;i<v.size();i++)
-        // {
-        //     v[i]=1;
-
-        //     cout<<v[i]<<" ";
-
-        //     ans=ans+pow(2,k);
-        //     k++;
-        // }
 
         return ans;
 
