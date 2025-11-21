8717d301729dc95e374a18c86c7ffb8a896a6d07
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2025-11-21 07:36:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+
+
+        map<char,vector<int>> m;
+
+        for(int i=0;i<s.length();i++)
+        {
+            m[s[i]].push_back(i);
+        }
+
+        set<string> st;
+
+        for(auto i:m)
+        {
+            vector<int> v=i.second;
+            if(v.size()>=2)
+            {
+                if(v[v.size()-1]-v[0]-1>0)
+                {
+                    string str="";
+                    str+=s[v[0]];
+                    
+                    for(int j=v[0]+1;j<v[v.size()-1];j++)
+                    {
+                        str+=s[j];
+                        str+=s[v[v.size()-1]];
+
+                        st.insert(str);
+                        cout<<str<<endl;
+                        str="";
+                        str=s[v[0]];
+                    }
+
+                }
+            }
+        }
+
+        return st.size();
+        
+    }
+};
