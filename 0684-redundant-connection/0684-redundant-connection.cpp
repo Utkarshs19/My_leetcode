7308560348dc43cd5e8ca448667e8d7fb88c7e0b
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2025-12-27 15:58:52 +0530

    Time: 95 ms (5.03%), Space: 37.8 MB (5.29%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+
+    void dfs(int node,vector<int> &vis,vector<vector<int>> &adj,int a,int b)
+    {
+        vis[node]=1;
+
+        for(auto i:adj[node])
+        {
+            if(node==a && i==b || node==b&&i==a)continue;
+
+            if(vis[i]==-1)
+            {
+                dfs(i,vis,adj,a,b);
+            }
+
+        }
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+
+        int x=-1;
+        int y=-1;
+
+        vector<int> ans;
+
+        int n=edges.size();
+
+        vector<vector<int>> adj(n+1);
+
+        for(int i=0;i<edges.size();i++)
+        {
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            int cc=0;
+
+            vector<int> vis(n+1,-1);
+
+            for(int j=1;j<n+1;j++)
+            {
+                if(vis[j]==-1)
+                {
+                    cc++;
+                    dfs(j,vis,adj,edges[i][0],edges[i][1]);
+                }
+            }
+
+            cout<<edges[i][0]<<"  "<<edges[i][1]<<"  "<<cc<<endl;
+
+            if(cc==1)
+            {
+                x=edges[i][0];
+                y=edges[i][1];
+            }
+        }
+        
+        ans.push_back(x);
+        ans.push_back(y);
+
+        return ans;
+    }
+};
