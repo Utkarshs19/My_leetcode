16a187d96ec5812590450c9c4b17ec7f7690f7c0
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2025-12-31 17:19:06 +0530

    Time: 55 ms (77.13%), Space: 134.3 MB (54.52%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
 
-    map<pair<int,int>,int> m;
     int ans=INT_MAX;
 
-    void dfs(int node,vector<vector<int>> &adj,vector<int> &vis)
+    void dfs(int node,vector<vector<pair<int,int>>> &adj,vector<int> &vis)
     {
         vis[node]=1;
 
         for(auto &i:adj[node])
-        {
-            ans=min(ans,m[{node,i}]);
-            ans=min(ans,m[{i,node}]);
-                
-            if(vis[i]==-1)
+        {        
+            ans=min(ans,i.second);
+                     
+            if(vis[i.first]==-1)
             {
 
-                dfs(i,adj,vis);
+                dfs(i.first,adj,vis);
             }
         }
     }
 
     int minScore(int n, vector<vector<int>>& roads) {
 
-        
-
-        vector<vector<int>> adj(n+1);
+        vector<vector<pair<int,int>>> adj(n+1);
 
         for(int i=0;i<roads.size();i++)
         {
-            adj[roads[i][0]].push_back(roads[i][1]);
-            adj[roads[i][1]].push_back(roads[i][0]);
-            m[{roads[i][0],roads[i][1]}]=roads[i][2];
-            m[{roads[i][1],roads[i][0]}]=roads[i][2];
+            adj[roads[i][0]].push_back({roads[i][1],roads[i][2]});
+            adj[roads[i][1]].push_back({roads[i][0],roads[i][2]});
+           
         }
         vector<int> vis(n+1,-1);
 
