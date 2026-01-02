bbb31843213f12ace2ef21700b74ac251dfe95b9
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-01-02 16:07:16 +0530

    Time: 389 ms (5.19%), Space: 117.8 MB (49.35%) - LeetHub

---
+++ 1487-making-file-names-unique/1487-making-file-names-unique.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<string> getFolderNames(vector<string>& names) {
+
+        vector<string> ans;
+
+        map<string,int> m;
+
+        for(int i=0;i<names.size();i++)
+        {
+            if(m.find(names[i])==m.end())
+            {
+                m[names[i]]=1;
+                ans.push_back(names[i]);
+            }
+            else
+            {
+                int k=m[names[i]];
+                while(true)
+                {
+                    string s=names[i];
+                    s+='(';
+                    s+=to_string(k);
+                    s+=')';
+
+                    if(m.find(s)==m.end())
+                    {
+                        ans.push_back(s);
+                        m[names[i]]=k+1;
+                        m[s]=1;
+                        k++;
+                        break;
+                    }
+                    k++;
+                }
+            }
+        }
+
+        return ans;
+        
+    }
+};
