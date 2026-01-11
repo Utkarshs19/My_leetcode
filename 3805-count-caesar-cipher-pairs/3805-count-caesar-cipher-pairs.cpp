081ec980007a8dc3edfd14c917064f4b20e219f5
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-01-11 12:02:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3805-count-caesar-cipher-pairs/3805-count-caesar-cipher-pairs.cpp
+++ 3805-count-caesar-cipher-pairs/3805-count-caesar-cipher-pairs.cpp
@@ -2,27 +2,46 @@ class Solution {
 public:
     long long countPairs(vector<string>& words) {
 
+        int n=words.size();
+        vector<long long> suf(n);
+
         long long ans=0;
 
-        unordered_map<string,long long> m;
+        map<string,int> m1;
+        suf[n-1]=0;
+        m1[words[n-1]]=n-1;
 
-        int n=words.size();
-
-        for(int i=n-1;i>=0;i--)
-        {
-            string str=words[i];
-            for(int j=0;j<26;j++)
+        for(int i=n-2;i>=0;i--)
             {
-                if(m.count(str))
-                {
-                    ans+=m[str];
-                }
-                for (char &c : str) {
-                    c = (c == 'z') ? 'a' : c + 1;
-                }
-            }
-            m[words[i]]++;
-        }
+                map<string,int> m;
+                string str=words[i];
+                for(int j=0;j<26;j++)
+                    {
+                        string s=str;
+                        for(int k=0;k<str.length();k++)
+                            {
+                                if(str[k]=='z')
+                                {
+                                    str[k]='a';
+                                }
+                                else
+                                {
+                                    str[k]=str[k]+1;
+                                }
+                            }
+                        m[str]++;
+
+                        if(m1.count(str))
+                        {
+                            ans+=suf[m1[str]]+1;
+                            suf[i]=suf[m1[str]]+1;
+                            m1.erase(str);
+                        }
+
+                    }   
+                
+                    m1[words[i]]=i;
+            }                
 
         return ans;
         
