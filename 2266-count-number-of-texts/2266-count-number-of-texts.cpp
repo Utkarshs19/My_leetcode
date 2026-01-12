0a45bc080c8de737a95fd99267d1ddc54ebd480b
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-01-12 17:51:15 +0530

    Time: 48 ms (13.47%), Space: 74.6 MB (6.94%) - LeetHub

--- 2266-count-number-of-texts/2266-count-number-of-texts.cpp
+++ 2266-count-number-of-texts/2266-count-number-of-texts.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     const int mod=1000000007;
 
-    set<string> st;
-    
-
-   vector<int> dp;
+    vector<int> dp;
        
 
     int helper(int i,string &s)
@@ -16,17 +13,13 @@ public:
 
         long long x=0;
 
-        if(i+1<=s.length() && st.count(s.substr(i,1)))
-        x=(x+helper(i+1,s))%mod;
+        int limit=(s[i]=='7' || s[i]=='9')?4:3;
 
-        if(i+2<=s.length() && st.count(s.substr(i,2)))
-        x=(x+helper(i+2,s))%mod;
-
-        if(i+3<=s.length() && st.count(s.substr(i,3)))
-        x=(x+helper(i+3,s))%mod;
-
-        if(i+4<=s.length() && st.count(s.substr(i,4)))
-        x=(x+helper(i+4,s))%mod;
+        for(int k=1;k<=limit && i+k<=s.length();k++)
+        {
+            if(s[i+k-1]!=s[i])break;
+            x=(x+helper(i+k,s))%mod;
+        }
 
         return dp[i]=x;
 
@@ -35,16 +28,6 @@ public:
 
     int countTexts(string pressedKeys) {
 
-    st.insert("2");    st.insert("22");    st.insert("222");    
-    st.insert("3");    st.insert("33");    st.insert("333");    
-    st.insert("4");    st.insert("44");    st.insert("444");   
-    st.insert("5");    st.insert("55");    st.insert("555");    
-    st.insert("6");    st.insert("66");    st.insert("666");
-    st.insert("7");    st.insert("77");    st.insert("777");    st.insert("7777");
-    st.insert("8");    st.insert("88");    st.insert("888");    
-    st.insert("9");    st.insert("99");    st.insert("999");    st.insert("9999");
-
-   
         dp.resize(100001,-1);
         return helper(0,pressedKeys);
         
