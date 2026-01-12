cf1038ffa07b9f84334d95f28d39e6cec779a96c
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-01-12 22:37:05 +0530

    Time: 48 ms (41.3%), Space: 319.3 MB (43.42%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -15,85 +15,49 @@ public:
 
         if(root==nullptr)return nullptr;
 
-        vector<vector<int>> v;
+      
 
         queue<TreeNode*> q;
         q.push(root);
-        v.push_back({root->val});
-
-        while(!q.empty())
-        {
-            int size=q.size();
-            vector<int> a;
-            
-            for(int i=0;i<size;i++)
-            {
-                TreeNode *node=q.front();
-                q.pop();
-
-                if(node->left && node->right)
-                {
-                    a.push_back(node->left->val+node->right->val);
-                }
-                else if(node->left && node->right==nullptr)
-                {
-                    a.push_back(node->left->val);
-                }
-                else if(node->right && node->left==nullptr)
-                {
-                    a.push_back(node->right->val);
-                }
-
-                if(node->left)
-                q.push(node->left);
-
-                if(node->right)
-                q.push(node->right);
-                
-            }
-            v.push_back(a);
-        }
-
-        
-        
-        q.push(root);
         root->val=0;
-        int k=1;
-
-        if(v.size()==1)return root;
 
         while(!q.empty())
         {
             int size=q.size();
+            long long sum=0;
 
-            int sum=accumulate(v[k].begin(),v[k].end(),0);
+            vector<TreeNode*> v;
 
             for(int i=0;i<size;i++)
             {
                 TreeNode* node=q.front();
+                v.push_back(node);
                 q.pop();
 
-                if(node->left && node->right)
-                {
-                    int x=node->left->val+node->right->val;
-                    node->left->val=sum-x;
-                    node->right->val=sum-x;
-                }
-                else if(node->left && node->right==nullptr)
-                {
-                    node->left->val=sum-node->left->val;
-                }
-                else if(node->right && node->left==nullptr)
-                {
-                    node->right->val=sum-node->right->val;
-                }
-                if(node->left)q.push(node->left);
-                if(node->right)q.push(node->right);
+                if(node->left)sum+=node->left->val;
+                if(node->right)sum+=node->right->val;
             }
-            k++;
+
+            for(auto i:v)
+            {
+                long long x=0;
+                if(i->left)x+=i->left->val;
+                if(i->right)x+=i->right->val;
+
+                if(i->left)
+                {
+                    i->left->val=sum-x;
+                    q.push(i->left);
+                }
+                if(i->right)
+                {
+                    i->right->val=sum-x;
+                    q.push(i->right);
+                }
+            }
+
         }
-
-
+        
 
         return root;
     }
