561fa74d1c17b2b8f2146e71a8c637ad1cfda8e2
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-01-23 11:11:37 +0530

    Time: 23 ms (79.86%), Space: 114.8 MB (40.28%) - LeetHub

--- 3128-right-triangles/3128-right-triangles.cpp
+++ 3128-right-triangles/3128-right-triangles.cpp
@@ -4,40 +4,21 @@ public:
 
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<pair<int,int>>> v1(n,vector<pair<int,int>>(m,{0,0})),v2(n,vector<pair<int,int>>(m,{0,0}));
+
+        vector<int> v1(n,0);
+        vector<int> v2(m,0);
 
        
 
 
         for(int i=0;i<grid.size();i++)
         {
-            int s=accumulate(grid[i].begin(),grid[i].end(),0);
-            int cnt=0;
             for(int j=0;j<grid[0].size();j++)
             {
                 if(grid[i][j]==1)
                 {
-                    s--;
-                    v1[i][j]={cnt,s};
-                    cnt++;
-                }
-            }
-        }
-
-        for(int i=0;i<grid[0].size();i++)
-        {
-            int s=0;
-            for(int j=0;j<grid.size();j++)
-            s+=grid[j][i];
-
-            int cnt=0;
-            for(int j=0;j<grid.size();j++)
-            {
-                if(grid[j][i]==1)
-                {
-                    s--;
-                   v2[j][i]={cnt,s};
-                    cnt++;
+                   v1[i]++;
+                   v2[j]++;
                 }
             }
         }
@@ -48,25 +29,14 @@ public:
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
-            {
-                
+            { 
                 if(grid[i][j]==1)
                 {
-                    int left=v1[i][j].first;
-                    int right=v1[i][j].second;
-
-                    int up=v2[i][j].first;
-                    int down=v2[i][j].second;
-
-                    ans+=(left*up);
-                    ans+=(left*down);
-                    ans+=(right*up);
-                    ans+=(right*down);
+                  ans+=((v1[i]-1)*(v2[j]-1));
                 }
 
             }
         }
-        
             
 
         return ans;
