4c9fcbaac1e406c9511ea3416f9ba8a406d109c5
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-01-28 13:14:41 +0530

    Time: 2 ms (36.48%), Space: 9.6 MB (36.86%) - LeetHub

---
+++ 1006-clumsy-factorial/1006-clumsy-factorial.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int clumsy(int n) {
+
+        vector<int> v;
+        int x=0;
+        while(n>0)
+        {
+            if(x==1 || x==2)
+            {
+                if(v.empty())
+                {
+                    v.push_back(n);
+                }
+                else
+                {
+                    int y=v[v.size()-1];
+                    v.pop_back();
+                    if(x==1)
+                    v.push_back(y*n);
+                    else if(x==2)
+                    {
+                        v.push_back(y/n);
+                    }
+                }
+            }
+            else
+            v.push_back(n);
+            x++;
+            n--;
+            if(x==4)x=0;
+
+        }
+        int ans=v[0];
+        bool flag=true;
+        for(int i=1;i<v.size();i++)
+        {
+            if(flag)ans+=v[i];
+            else
+            ans-=v[i];
+            flag=!flag;
+        }
+
+        return ans;
+
+
+
+        
+    }
+};
