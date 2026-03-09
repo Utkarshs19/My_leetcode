b9a964e1038bfec7ecfa132db0a586b6b38edc17
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-03-09 19:52:59 +0530

    Time: 39 ms (5.28%), Space: 8.5 MB (60.41%) - LeetHub

--- 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.cpp
+++ 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int ans=0;
-    vector<int> v;
     void helper(int i,int n,int limit)
     {
         if(i==3)
         {
-            if(accumulate(v.begin(),v.end(),0)==n)ans++;
+            if(n==0)ans++;
 
             return;
         }
 
         for(int j=0;j<=limit;j++)
         {
-            v[i]=j;
-            helper(i+1,n,limit);
+            helper(i+1,n-j,limit);
         }
     }
 
     int distributeCandies(int n, int limit) {
         
-        v.resize(3,0);
         helper(0,n,limit);
 
         return ans;
