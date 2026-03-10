906c0fc622f44a992b221319a29681cc786c2935
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-03-10 19:31:19 +0530

    Time: 0 ms (100%), Space: 13.9 MB (68.03%) - LeetHub

---
+++ 0789-escape-the-ghosts/0789-escape-the-ghosts.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool escapeGhosts(vector<vector<int>>& ghosts, vector<int>& target) {
+
+        int dist=abs(target[0])+abs(target[1]);
+
+        for(int i=0;i<ghosts.size();i++)
+        {
+            int x=abs(ghosts[i][0]-target[0])+abs(ghosts[i][1]-target[1]);
+
+            if(x<=dist)return false;
+        }
+
+        return true;
+        
+    }
+};
