acdb51d0860456af6fc191250eda2546aea2cdeb
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-03-13 13:35:20 +0530

    Time: 141 ms (70%), Space: 35.3 MB (67.73%) - LeetHub

---
+++ 3366-minimum-array-sum/3366-minimum-array-sum.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+   
+    long long dp[101][101][101];
+
+    long long helper(int i,vector<int> &nums,int k,int op1,int op2)
+    {
+       int n = nums.size();
+
+        op1 = min(op1, n - i);
+        op2 = min(op2, n - i);
+
+        if(i==nums.size())
+        {
+            return 0;
+        }
+
+
+        if(dp[i][op1][op2]!=-1)
+        return dp[i][op1][op2];
+
+        long long res=nums[i]+helper(i+1,nums,k,op1,op2);
+
+        if(op1>0)
+        {
+            res=min(res,(long long)((nums[i]+1)/2)+helper(i+1,nums,k,op1-1,op2));
+        }
+
+        if(op2>0)
+        {
+            if(nums[i]>=k)
+            res=min(res,(long long)(nums[i]-k)+helper(i+1,nums,k,op1,op2-1));
+        }
+
+        if(op1>0 && op2>0)
+        {
+            long long temp=(nums[i]+1)/2;
+            if(temp>=k)
+            {
+                res=min(res,(temp-k+helper(i+1,nums,k,op1-1,op2-1)));
+            }
+
+            temp=nums[i];
+            if(temp>=k)
+            {
+                temp=temp-k;
+
+                res=min(res,((temp+1)/2+helper(i+1,nums,k,op1-1,op2-1)));
+            }
+
+        }
+
+        return dp[i][op1][op2]=res;
+
+    }
+
+    int minArraySum(vector<int>& nums, int k, int op1, int op2) {
+        
+        memset(dp,-1,sizeof(dp));
+
+        return helper(0,nums,k,op1,op2);
+
+    }
+};
