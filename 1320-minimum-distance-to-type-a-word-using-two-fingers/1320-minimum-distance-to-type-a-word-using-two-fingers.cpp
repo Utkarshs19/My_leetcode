66b02a641dafabb04536d56a3826ded808ce344b
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-03-27 16:38:05 +0530

    Time: 5 ms (79.67%), Space: 9.4 MB (85.06%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -3,12 +3,9 @@ public:
 
     int cost(int i,int j)
     {
-        return abs(m['A'+i].first-m['A'+j].first)+
-                abs(m['A'+i].second-m['A'+j].second);
+        return abs(i/6-j/6)+
+                abs(i%6-j%6);
     }
-
-    map<char,pair<int,int>> m;
-
     int dp[27][27][301];
 
     int helper(int i,int j,int k,string &word)
@@ -33,35 +30,7 @@ public:
 
 
     int minimumDistance(string word) {
-
-        m['A']={0,0};
-        m['B']={0,1};
-        m['C']={0,2};
-        m['D']={0,3};
-        m['E']={0,4};
-        m['F']={0,5};
-        m['G']={1,0};
-        m['H']={1,1};
-        m['I']={1,2};
-        m['J']={1,3};
-        m['K']={1,4};
-        m['L']={1,5};
-        m['M']={2,0};
-        m['N']={2,1};
-        m['O']={2,2};
-        m['P']={2,3};
-        m['Q']={2,4};
-        m['R']={2,5};
-        m['S']={3,0};
-        m['T']={3,1};
-        m['U']={3,2};
-        m['V']={3,3};
-        m['W']={3,4};
-        m['X']={3,5};
-        m['Y']={4,0};
-        m['Z']={4,1};
-
-
+        
         int ans=INT_MAX;
 
         memset(dp,-1,sizeof(dp));
