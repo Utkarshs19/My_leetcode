8a818b61753d7171324fc65fb810d63e99ab8042
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-03-28 19:16:57 +0530

    Time: 0 ms (100%), Space: 14.6 MB (87.36%) - LeetHub

--- 1958-check-if-move-is-legal/1958-check-if-move-is-legal.cpp
+++ 1958-check-if-move-is-legal/1958-check-if-move-is-legal.cpp
@@ -4,117 +4,30 @@ public:
         int n=board.size();
         int m=board[0].size();
         board[rMove][cMove]=color;
-        int cnt=0;
-
-        for(int i=rMove-1;i>=0;i--)
-        {
-            if(board[i][cMove]!=board[rMove][cMove] && board[i][cMove]!='.')
-            cnt++;
-            else
-            {
-                if(board[i][cMove]==color && cnt>=1)return true;
-                  break;
-            }
-        }
-        cnt=0;
-        for(int i=rMove+1;i<n;i++)
-        {
-            if(board[i][cMove]!=board[rMove][cMove] && board[i][cMove]!='.')
-            cnt++;
-            else
-            {
-                if(board[i][cMove]==color && cnt>=1)return true;
-                  break;
-            }
-        }
-
-        cnt=0;
-        for(int i=cMove-1;i>=0;i--)
-        {
-            if(board[rMove][i]!=board[rMove][cMove] && board[rMove][i]!='.')
-            cnt++;
-            else
-            {
-                if(board[rMove][i]==color && cnt>=1)return true;
-                  break;
-            }
-        }
-        cnt=0;
-        for(int i=cMove+1;i<m;i++)
-        {
-            if(board[rMove][i]!=board[rMove][cMove] && board[rMove][i]!='.')
-            cnt++;
-            else
-            {
-                if(board[rMove][i]==color && cnt>=1)return true;
-                break;
-            }
-        }
-
-        int i=rMove-1,j=cMove-1;
-        cnt=0;
-        while(i>=0 && j>=0)
-        {
-            if(board[i][j]!=board[rMove][cMove] && board[i][j]!='.')
-            cnt++;
-            else
-            {
-                if(board[i][j]==color && cnt>=1)return true;
-                  break;
-            }
-            i--;
-            j--;
-        }
-
-        i=rMove+1,j=cMove+1;
-        cnt=0;
-        while(i<n && j<m)
-        {
-            if(board[i][j]!=board[rMove][cMove] && board[i][j]!='.')
-            cnt++;
-            else
-            {
-                if(board[i][j]==color && cnt>=1)return true;
-                  break;
-            }
-            i++;
-            j++;
-        }
-
-        i=rMove-1,j=cMove+1;
-        cnt=0;
-        while(i>=0 && j<m)
-        {
-            if(board[i][j]!=board[rMove][cMove] && board[i][j]!='.')
-            cnt++;
-            else
-            {
-                if(board[i][j]==color && cnt>=1)return true;
-                  break;
-            }
-            i--;
-            j++;
-        }
-
-        i=rMove+1,j=cMove-1;
-        cnt=0;
-
-        while(i<n && j>=0)
-        {
-            if(board[i][j]!=board[rMove][cMove] && board[i][j]!='.')
-            cnt++;
-            else
-            {
-                if(board[i][j]==color && cnt>=1)return true;
-                  break;
-            }
-            i++;
-            j--;
-        }
         
 
+        int dirs[8][2]={{-1,0},{1,0},{0,-1},{0,1},
+                          {-1,-1},{1,1},{-1,1},{1,-1}};
+
+        for(auto d:dirs)
+        {
+            int i=rMove+d[0];
+            int j=cMove+d[1];
+            int cnt=0;
+
+            while(i>=0 && j>=0 && i<n && j<m)
+            {
+                if(board[i][j]!=color && board[i][j]!='.')
+                cnt++;
+                else
+                {
+                    if(board[i][j]==color && cnt>=1)return true;
+                    break;
+                }
+                i+=d[0];
+                j+=d[1];
+            }
+        }
         return false;
-
-        
     }
 };
