1bf1cd96da6be8f6a48e8758d21662421d3fc912
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-04-10 13:56:03 +0530

    Time: 30 ms (16.54%), Space: 70.4 MB (97.99%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -2,29 +2,19 @@ class Solution {
 public:
 
     bool helper(int mid,vector<int> &bloomDay,int m,int k)
-    {
-        vector<int> v(bloomDay.size(),0);
+    { 
+        int cnt=0,cnt1=0;
 
         for(int i=0;i<bloomDay.size();i++)
         {
-            if(bloomDay[i]<=mid)v[i]=1;
-        }
-
-        int cnt=0;
-
-        int i=0,j=0;
-        int cnt1=0;
-        while(j<v.size())
-        {
-            if(v[j]==1)cnt1++;
-            else
-            {
-                cnt+=(cnt1/k);
-                cnt1=0;
+            if(bloomDay[i]<=mid)cnt1++;
+            else{
+            cnt+=(cnt1/k);
+            cnt1=0;
             }
-            j++;
         }
         cnt+=(cnt1/k);
+
         return cnt>=m;
     }
 
