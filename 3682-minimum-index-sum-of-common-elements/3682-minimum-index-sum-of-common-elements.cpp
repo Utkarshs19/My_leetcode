396c02e3f6567ab92668b8aec1c6dd5872d6a561
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-05-22 20:39:32 +0530

    Time: 376 ms (28.57%), Space: 276.8 MB (57.14%) - LeetHub

--- 3682-minimum-index-sum-of-common-elements/3682-minimum-index-sum-of-common-elements.cpp
+++ 3682-minimum-index-sum-of-common-elements/3682-minimum-index-sum-of-common-elements.cpp
@@ -2,26 +2,23 @@ class Solution {
 public:
     int minimumSum(vector<int>& nums1, vector<int>& nums2) {
 
-        map<int,vector<int>> m;
+        map<int,int> m;
 
         int ans=1e9;
 
         for(int i=0;i<nums1.size();i++)
         {
-            m[nums1[i]].push_back(i);
+            if(!m.count(nums1[i]))
+            {
+                m[nums1[i]]=i;
+            }
         }
 
         for(int i=0;i<nums2.size();i++)
         {
             if(m.count(nums2[i]))
             {
-                vector<int> v=m[nums2[i]];
-
-                for(int j=0;j<v.size();j++)
-                {
-                    ans=min(ans,i+v[j]);
-                    if(ans==0)return ans;
-                }
+                ans=min(ans,i+m[nums2[i]]);
             }
         }
 
