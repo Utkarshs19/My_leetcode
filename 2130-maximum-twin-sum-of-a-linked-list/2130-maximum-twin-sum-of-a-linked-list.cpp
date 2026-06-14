af0e8b09948efac758f852e0b96dfcf33d91e4dc
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-06-14 08:46:16 +0530

    Time: 879 ms (5.13%), Space: 124.3 MB (67.45%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode* reverse_list(ListNode *head)
+    {
+        // cout<<head->val<<"  "<<head->next->val<<endl;
+        ListNode *d=nullptr;
+        ListNode *n=nullptr;
+        while(head){
+        n=head->next;
+        head->next=d;
+        d=head;
+        head=n;
+        }
+
+        ListNode *p=d;
+        while(p)
+        {
+            cout<<p->val<<" ";
+            p=p->next;
+        }
+
+        return d;
+    }
+
+    int pairSum(ListNode* head) {
+
+        ListNode *slow=head;
+        ListNode *fast=head;
+
+        while(fast&&fast->next)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        slow=reverse_list(slow);
+        int mini=INT_MIN;
+        while(slow)
+        {
+            if(mini<(slow->val+head->val))
+            mini=slow->val+head->val;
+            slow=slow->next;
+            head=head->next;
+
+        }
+
+        return mini;
+
+        
+        
+    }
+};
