8987135fbacd6f8ec636435c8d4a7e72fbb85021
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-06-27 08:40:33 +0530

    Time: 1294 ms (5.06%), Space: 17.7 MB (34.83%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -3,22 +3,26 @@ public:
     int longestSubstring(string s, int k) {
 
         int ans=0;
-        map<char,int> m;
 
         for(int i=0;i<s.length();i++)
         {
-           m[s[i]]++;
-           int flag=0;
-           for(auto i:m)
-           {
-               if(i.second<k)
-               flag=-1;
-           }
-           if(flag==0)
-           ans=max(ans,i+1);
+            map<char,int> m;
+            for(int j=i;j<s.length();j++)
+            {
+                m[s[j]]++;
+                int mini=INT_MAX;
+                for(auto i:m)
+                {
+                    mini=min(mini,i.second);
+                }
+                if(mini>=k)
+                {
+                    ans=max(ans,(int)(j-i+1));
+                }
+            }
         }
+        
 
         return ans;
-        
     }
 };
