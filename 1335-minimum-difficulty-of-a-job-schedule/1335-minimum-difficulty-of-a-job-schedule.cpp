ddb7104ac9c3e702d3657f1d11bfe56d9b0782a4
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-09-16 16:31:22 +0530

    Time: 52 ms (28.35%), Space: 10.3 MB (87.05%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int dp[301][11];
+    int n;
+    int helper(int i,vector<int> &jobDifficulty,int d)
+    {
+        if(d==1)
+        {
+            int maxi=0;
+            for(int j=i;j<n;j++)
+            {
+                maxi=max(maxi,jobDifficulty[j]);
+            }
+            return maxi;
+        }
+        if(dp[i][d]!=-1)return dp[i][d];
+
+        int res=INT_MAX;
+        int maxi=jobDifficulty[i];
+        for(int j=i;j<=n-d;j++)
+        {
+            maxi=max(maxi,jobDifficulty[j]);
+
+            int result=maxi+helper(j+1,jobDifficulty,d-1);
+
+            res=min(res,result);
+        }
+
+        return dp[i][d]= res;
+
+    }
+    int minDifficulty(vector<int>& jobDifficulty, int d) {
+        this->n=jobDifficulty.size();
+
+        if(n<d)return -1;
+        memset(dp,-1,sizeof(dp));
+        return helper(0,jobDifficulty,d);
+    }
+};
