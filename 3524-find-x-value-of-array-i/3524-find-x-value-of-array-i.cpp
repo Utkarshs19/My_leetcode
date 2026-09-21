480aec00042f60dbe1d5ae57f414ba6a76a51687
Utkarsh Sharma <95514959+Utkarshs19@users.noreply.github.com>
2026-09-21 15:53:29 +0530

    Time: 156 ms (66.07%), Space: 151 MB (81.25%) - LeetHub

---
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<long long> resultArray(vector<int>& nums, int k) {
+
+        vector<long long> ans(k,0); 
+        vector<long long> prev(k,0);
+
+        for(int i=0;i<nums.size();i++)
+        {
+            vector<long long> cur(k,0);
+            cur[nums[i]%k]++;
+
+            for(int j=0;j<k;j++)
+            {
+                long long x=(1ll * j*(nums[i]%k))%k;
+                
+                cur[x]+=prev[j];
+            }
+
+            for(int j=0;j<k;j++)
+            {
+                ans[j]+=cur[j];
+            }
+            prev=std::move(cur);
+        }
+
+        return ans;
+        
+    }
+};
